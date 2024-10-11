a3e12836a87d7174b49aefec9b7fa157e7a39dfd
Ben <bao.nguyen@xpodc.com>
2024-10-11 10:36:14 +0700

    setup connected to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,32 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { config as envConfig } from 'dotenv';
+envConfig();
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        const portStr = process.env.DB_PORT;
+        if (!portStr) {
+          throw new Error('process.env.DB_PORT is not defined');
+        }
+        const port = parseInt(portStr, 10);
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: port,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          synchronize: true,
+        };
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
