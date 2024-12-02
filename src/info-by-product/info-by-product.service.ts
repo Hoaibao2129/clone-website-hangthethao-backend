00ce4a1248851a8443fe71043b4ae5700e1bae12
Ben <bao.nguyen@xpodc.com>
2024-12-02 22:20:02 +0700

    add feature get info buy Prod

--- src/info-by-product/info-by-product.service.ts
+++ src/info-by-product/info-by-product.service.ts
@@ -1,22 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { InfoBuyProduct } from 'entities/infoBuyProduct.entity';
-import { Repository } from 'typeorm';
+
 @Injectable()
-export class InfoByProductService {
-    constructor(
-        @InjectRepository(InfoBuyProduct)
-        private infoBuyProd: Repository<InfoBuyProduct>,
-    ) { }
-
-
-    async getInfoBuyProduct() {
-        const getInfoBuyProduct = await this.infoBuyProd.find();
-        return getInfoBuyProduct;
-    }
-
-    async getInfoBuyProductById(id: number) {
-        const getInfoBuyProduct = await this.infoBuyProd.findOne({ where: { id } });
-        return getInfoBuyProduct;
-    }
-}
+export class InfoByProductService {}
